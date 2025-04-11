321c39b36943dec902f2fa728546f7bda61d714a
inischal <nspnischal@gmail.com>
2025-04-11 17:02:06 +0545

    routing functionality and Link added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.webp";
 import cart_icon from "../../assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
@@ -17,33 +18,52 @@ const Navbar = () => {
             setMenu("home");
           }}
         >
-          Home{menu === "home" ? <hr /> : <></>}
+          <Link style={{ textDecoration: "none", color: "#626262" }} to="/">
+            Home
+          </Link>
+          {menu === "home" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("mens");
           }}
         >
-          Men{menu === "mens" ? <hr /> : <></>}
+          <Link style={{ textDecoration: "none", color: "#626262" }} to="/mens">
+            Men
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("womens");
           }}
         >
-          Women{menu === "womens" ? <hr /> : <></>}
+          <Link
+            style={{ textDecoration: "none", color: "#626262" }}
+            to="/womens"
+          >
+            Women
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids{menu === "kids" ? <hr /> : <></>}
+          <Link style={{ textDecoration: "none", color: "#626262" }} to="/kids">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="" srcset="" height="40px" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="" srcset="" height="40px" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
